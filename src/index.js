3159e10b5f4c080eabfbfcf6c14e11d317bf04c9
eca0303 <emma.c.dsilva@gmail.com>
2025-06-27 00:41:11 +1000

    removed the console log

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-     console.log("poem generated");
     new Typewriter("#poem", {
         strings: response.data.answer,
         autoStart: true,
@@ -8,10 +7,10 @@ function displayPoem(response) {
     });
 
 }
+
 function generatePoem(event) {
     event.preventDefault();
 
-    // Build API URL 
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "370ab147d13obce21d786caff668ftb9";
     let context = "You are a creative poem expert. Generate a 4-line HTML-formatted poem only using <br> tags, and do not wrap it in code blocks or Markdown. Make sure to follow the user instructions. Do not include title in poems. Sign the Poem SheCodes AI in bold inside a <strong> element on the last line.";
@@ -23,13 +22,8 @@ function generatePoem(event) {
     poemElement.classList.remove("hidden");
     poemElement.innerHTML = `<div class="generating">⏳Generating a Short poem about ${instructionsInput.value}</div>`;
     
-    console.log("Generating Poem")
-    console.log(`Prompt: ${prompt}`);
-    console.log(`Context: ${context}`);
-    // Make a call to th API
     axios.get(apiUrl).then(displayPoem);
 
-    // Display the generated poem
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
